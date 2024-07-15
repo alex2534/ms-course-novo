e418d76913be5e5634a83ea400d034e565ef35fa
alex2534 <alex2534@outlook.com>
2024-07-15 16:28:51 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -0,0 +1,40 @@
+package com.devsuperior.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+
+ 
+ 
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserRepository respository;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> fidById(@PathVariable Long id) {
+		
+		User obj = respository.findById(id).get();
+		return ResponseEntity.ok(obj);
+		
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> fidById(@RequestParam String email) {
+		
+		User obj = respository.findByEmail(email);
+		return ResponseEntity.ok(obj);
+		
+	}
+	
+}
