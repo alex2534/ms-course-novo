b4f135b5160a58f652e55c7e303573708932ad32
alex2534 <alex2534@outlook.com>
2024-07-15 14:35:58 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
@@ -0,0 +1,73 @@
+package com.devsuperior.hruser.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String RoleName;
+	
+	public Role() {
+		
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		RoleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return RoleName;
+	}
+
+	public void setRoleName(String roleName) {
+		RoleName = roleName;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", RoleName=" + RoleName + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(RoleName, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(RoleName, other.RoleName) && Objects.equals(id, other.id);
+	}
+	
+	
+
+}
